636f72299a48a2cbeb361b6cdd5f732a5f960d8b
jian-jiang <jianan.jiang@student.unsw.edu.au>
2015-12-08 20:43:49 +1100

    local read support

--- js/collapse.js
+++ js/collapse.js
@@ -6,11 +6,12 @@
 		$('span.sectionHead').click(function(evt){
 			var ol = $(this).parent().next("ol");
 			ol.toggleClass("hide");
-			if(ol.hasClass("hide")){
-				$(this).find("img").attr("src","/images/expand_all.gif");
-			}else{
-				$(this).find("img").attr("src","/images/collapse_all.gif");
-			}
+			// if(ol.hasClass("hide")){
+			// 	$(this).find("img").attr("src","/images/expand_all.gif");
+			// }else{
+			// 	$(this).find("img").attr("src","/images/collapse_all.gif");
+			// }
+			$(this).find("img").toggleClass("hide");
 		});
 
 		$('#toggleAll').click(function(){
@@ -21,22 +22,21 @@
 
 			if(_t){
 				$(this).find("span").text("展开所有");
-				$(this).find("img").attr("src","/images/expand_all.gif");
 			}else{
 				$(this).find("span").text("折叠所有");
-				$(this).find("img").attr("src","/images/collapse_all.gif");
 			}
+
+			$(this).find("img").toggleClass("hide");
 
 			for(var i = 0; i < spans.length; i++){
 				span = spans[i];
 				var ol = $(span).parent().next("ol");
 				if(_t){
 					ol.addClass("hide");
-					$(span).find("img").attr("src","/images/expand_all.gif");
 				}else{
 					ol.removeClass("hide");
-					$(span).find("img").attr("src","/images/collapse_all.gif");
 				}
+				$(span).find("img").toggleClass("hide");
 			}
 		});
 	});
